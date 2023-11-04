e92e8632acdd53e7f75519b451d67e606959fa20
Pranav Gogineni <pranavgv@gmail.com>
2023-11-04 17:11:55 -0400

    Adds options to have the airline also retrieve the reviews data along with the json

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -4,20 +4,20 @@ module Api
       def index
         airlines = Airline.all
 
-        render json: AirlineSerializer.new(airlines).serialized_json
+        render json: AirlineSerializer.new(airlines, options).serialized_json
       end
 
       def show
         airline = Airline.find_by(slug: params[:slug])
 
-        render json: AirlineSerializer.new(airline).serialized_json
+        render json: AirlineSerializer.new(airline, options).serialized_json
       end
 
       def update 
         airline = Airline.find_by(slug: params[:slug])
 
         if airline.update(airline_params)
-          render json: AirlineSerializer.new(airline).serialized_json
+          render json: AirlineSerializer.new(airline, options).serialized_json
         else
           render json: {error: airline.errors.messages}, status: 422
         end
@@ -44,6 +44,10 @@ module Api
 
       def airline_params
         params.require(:airline).permit(:name, :image_url)
+      end
+
+      def options
+        @options ||= { include: %i[reviews] }
       end
     end
   end
