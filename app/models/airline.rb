6f8ff9e0d0e0d89ca5ef9c52dc6159416f550845
Pranav Gogineni <pranavgv@gmail.com>
2023-11-04 16:20:19 -0400

    Adds average score function

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -6,4 +6,10 @@ class Airline < ApplicationRecord
   def slugify
     self.slug = name.parameterize
   end
+
+  def avg_score 
+    return 0 unless reviews.size.positive?
+
+    reviews.average(:score).to_f.round(2)
+  end
 end
